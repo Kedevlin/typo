0e026494c1827798b8eb76579e8bcce57270f74d
Kelly <kelly_devlin@alumni.brown.edu>
2016-03-29 13:39:53 -0700

    utilize the save method without the bang in conditional

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -47,8 +47,7 @@ class Admin::CategoriesController < Admin::BaseController
   end
 
   def save_category
-    @category.save
-    if @category.errors.empty?
+    if @category.save
       flash[:notice] = _('Category was successfully saved.')
     else
       flash[:error] = _('Category could not be saved.')
